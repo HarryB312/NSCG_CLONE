9964ab016a2eb4e974b835a4ea14e275d8850f44
harrison boswell <harryb@harrisons-MBP.lan>
2024-05-11 12:44:39 -0400

    Updated some CSS files

--- src/Pages/Expertise.jsx
+++ src/Pages/Expertise.jsx
@@ -9,29 +9,29 @@ export default function Expertise(){
           <p>519130 Internet Publishing and Broadcasting and Web Search Portal</p>
           <p>541490 Other Specialized Design Services</p>
           <p>541511 Custom Computer Programming Services</p>
-          <p> 541513 Computer Facilities Management Services</p>
-          <p> 541519 Other Computer Related Services</p>
+          <p>541513 Computer Facilities Management Services</p>
+          <p>541519 Other Computer Related Services</p>
           <p>541611 Administrative Management and General Management Consulting Services</p>
-          <p> 541612 Human Resources Consulting Services</p>
-          <p> 541614 Process, Physical Distribution, and Logistics Consulting Services</p>
-          <p> 541618 Other Management Consulting Services</p>
-          <p> 541690 Other Scientific and Technical Consulting Services</p>
-          <p> 541720 Research and Development in the Social Sciences and Humanities</p>
-          <p> 541990 All Other Professional, Scientific, and Technical Services</p>
-          <p>  561110 Office Administrative Services</p>
-          <p>  561210 Facilities Support Services</p>
-          <p>  561311 Employment Placement Agencies</p>
-          <p>  561320 Temporary Help Services</p>
-          <p>  561410 Document Preparation Services</p>
-          <p>  561421 Telephone Answering Services</p>
-          <p>  561422 Telemarketing Bureaus and Other Contact Centers</p>
-          <p>  561499 All Other Business Support Services</p>
-          <p> 561990 All Other Support Services</p>
-          <p> 611410 Business and Secretarial Schools</p>
+          <p>541612 Human Resources Consulting Services</p>
+          <p>541614 Process, Physical Distribution, and Logistics Consulting Services</p>
+          <p>541618 Other Management Consulting Services</p>
+          <p>541690 Other Scientific and Technical Consulting Services</p>
+          <p>541720 Research and Development in the Social Sciences and Humanities</p>
+          <p>541990 All Other Professional, Scientific, and Technical Services</p>
+          <p>561110 Office Administrative Services</p>
+          <p>561210 Facilities Support Services</p>
+          <p>561311 Employment Placement Agencies</p>
+          <p>561320 Temporary Help Services</p>
+          <p>561410 Document Preparation Services</p>
+          <p>561421 Telephone Answering Services</p>
+          <p>561422 Telemarketing Bureaus and Other Contact Centers</p>
+          <p>561499 All Other Business Support Services</p>
+          <p>561990 All Other Support Services</p>
+          <p>611410 Business and Secretarial Schools</p>
           <p>611420 Computer Training</p>
-          <p> 611699 All Other Miscellaneous Schools and Instruction</p>
-          <p> 624190 Other Individual and Family Services</p>
-          <p> 711510 Independent Artists, Writers, and Performers</p>
+          <p>611699 All Other Miscellaneous Schools and Instruction</p>
+          <p>624190 Other Individual and Family Services</p>
+          <p>711510 Independent Artists, Writers, and Performers</p>
         <div className="email-container">
           <h2>INTERESTED IN DOING BUSINESS WITH NSCG?</h2>
           <button>Email Us</button>
